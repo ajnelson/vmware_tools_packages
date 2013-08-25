8f5bd8798bfcaad5bb02510876a29566a6374860
Alex Nelson <ajnelson@acm.org>
2013-08-25 15:37:40 -0400

    Try a few softlinks in Ubuntu 13.04, to no avail

--- ubuntu-13.04.sh
+++ ubuntu-13.04.sh
@@ -3,8 +3,10 @@
 set -e
 set -x
 
-# This worked out of the box.
-sudo apt-get install build-essential kernel-headers-$(uname -r)
+#Haven't gotten this working yet.
+false
+
+sudo apt-get install build-essential linux-headers-$(uname -r)
 
 #C/o http://askubuntu.com/questions/231876/vmware-cant-find-linux-headers-path
 #Unfortunately, this is still insufficient.  There are compiler errors indicative of some header not making it in (implicit symbol definitions).
@@ -13,14 +15,17 @@ sudo apt-get install build-essential kernel-headers-$(uname -r)
 #  /usr/src/linux-headers-$(uname -r)/include/linux/version.h
 
 #C/o http://sysadminramblings.wordpress.com/2013/05/28/vmware-tools-install-ubuntu-13-04/
-pushd /lib/modules/$(uname -r)/build/include/linux
-if [ ! -r utsrelease.h ]; then
-  sudo ln -s ../generated/utsrelease.h
-fi
-if [ ! -r autoconf.h ]; then
-  sudo ln -s ../generated/autoconf.h
-fi
-if [ ! -r version.h ]; then
-  sudo ln -s ../generated/uapi/linux/version.h
-fi
-popd
+#The first path came from the sysadminramblings site, didn't work; the second path tried the same approach with the askubuntu response from above, also didn't work. Shotgunning both sets of symlinks didn't work.
+for path in /lib/modules/$(uname -r)/build/include/linux /usr/src/linux-headers-$(uname -r)/include/linux; do
+  pushd $path
+  if [ ! -r utsrelease.h ]; then
+    sudo ln -s ../generated/utsrelease.h
+  fi
+  if [ ! -r autoconf.h ]; then
+    sudo ln -s ../generated/autoconf.h
+  fi
+  if [ ! -r version.h ]; then
+    sudo ln -s ../generated/uapi/linux/version.h
+  fi
+  popd
+done
